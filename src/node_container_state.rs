3aafab9f09443ebce51ef8cc77bd548a42390ec1
Lukas Orsvarn <lukas@orsvarn.com>
2020-04-23 14:34:08 +0200

    WIP generate nodes from list

--- src/node_container_state.rs
+++ src/node_container_state.rs
@@ -2,7 +2,33 @@ use orbtk::prelude::*;
 
 #[derive(Default, AsAny)]
 pub struct NodeContainerState {
+    builder: WidgetBuildContext,
+    count: usize,
+    node_container: Entity,
 }
 
 impl State for NodeContainerState {
+    fn init(&mut self, _: &mut Registry, ctx: &mut Context) {
+        self.node_container = ctx
+            .entity_of_child("node_container")
+            .expect("ListViewState.init: ItemsPanel child could not be found.");
+    }
+
+    fn update(&mut self, _: &mut Registry, ctx: &mut Context<'_>) {
+        let count = ctx.widget().clone_or_default::<usize>("count");
+
+        if count != self.count {
+            if let Some(builder) = &self.builder {
+                ctx.clear_children_of(self.node_container);
+
+                for i in 0..count {
+                    let build_context = &mut ctx.build_context();
+                    let item = ListViewItem::create().build(build_context);
+                    build_context.append_child(self.node_container, item);
+                }
+            }
+
+            self.count = count;
+        }
+    }
 }
--- src/node_container_view.rs
+++ src/node_container_view.rs
@@ -18,15 +18,10 @@ impl Template for NodeContainerView {
             right: 0.,
             bottom: 0.,
         };
-        let my_margin_b = Thickness{
-            left: 130.,
-            top: 30.,
-            right: 0.,
-            bottom: 0.,
-        };
         self.name("NodeContainerView")
             .child(
                 Container::create()
+                    .id("node_container")
                     .child(
                         NodeView::create()
                         .title("My node")
@@ -35,15 +30,38 @@ impl Template for NodeContainerView {
                     )
                 .build(ctx)
             )
-            .child(
-                Container::create()
-                    .child(
-                        NodeView::create()
-                        .title("My node 2")
-                        .my_margin(my_margin_b)
-                        .build(ctx),
-                    )
-                .build(ctx)
-            )
+        // let my_margin_a = Thickness{
+        //     left: 10.,
+        //     top: 0.,
+        //     right: 0.,
+        //     bottom: 0.,
+        // };
+        // let my_margin_b = Thickness{
+        //     left: 130.,
+        //     top: 30.,
+        //     right: 0.,
+        //     bottom: 0.,
+        // };
+        // self.name("NodeContainerView")
+        //     .child(
+        //         Container::create()
+        //             .child(
+        //                 NodeView::create()
+        //                 .title("My node")
+        //                 .my_margin(my_margin_a)
+        //                 .build(ctx),
+        //             )
+        //         .build(ctx)
+        //     )
+        //     .child(
+        //         Container::create()
+        //             .child(
+        //                 NodeView::create()
+        //                 .title("My node 2")
+        //                 .my_margin(my_margin_b)
+        //                 .build(ctx),
+        //             )
+        //         .build(ctx)
+        //     )
     }
 }
